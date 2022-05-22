f355bdf5a20f149f45aef00c412259e60eb5e1b6
Alia Abouatie <abouatiealia@gmail.com>
2022-05-22 16:05:15 +0200

    Statistik Versuch

--- TikTakToe/src/statistic/Combi.java
+++ TikTakToe/src/statistic/Combi.java
@@ -2,8 +2,14 @@ package statistic;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
+import java.nio.StandardOpenOption;
 import statistic.Player;
-
 
 public class Combi {
     private String Name;
@@ -16,7 +22,9 @@ public class Combi {
         this.Spieler2=Spieler2;
         this.Ausgang=Ausgang;
         }*/
-
+    public void SpielerInCSV(String Player,boolean Spielstand){
+        BufferedWriter meinWriter= Files.newBufferedWriter((csv, StandardOpenOption.APPEND));
+    }ard
         // Versuch aktuellen Spielstand ==> wie aktualisieren wir in csv?
         public void fuegeAktlSpielstand(String Player, String Spielstand) {
             int win = 0;
@@ -33,6 +41,7 @@ public class Combi {
             this.Name = Name;
 
 
+
             try (PrintWriter writer = new PrintWriter("statistic.csv")) {
 
                 StringBuilder sb = new StringBuilder();
@@ -43,6 +52,7 @@ public class Combi {
                 ;
                 sb.append(Name);
                 sb.append(Ausgang);
+                Spielstand.toString
                 writer.write(sb.toString());
 
                 System.out.println("done!");
@@ -51,6 +61,39 @@ public class Combi {
                 System.out.println(e.getMessage());
             }
 
+            try {
+                Path csv= Paths.get("/Users/aliaabouatie/java_project/statistic.csv");
+
+                BufferedReader meinReader= Files.newBufferedReader(csv);
+                int zeilenCounter= 1;
+
+                String zeile= meinReader.readLine();
+
+                zeile =  meinReader.readLine();
+                /*String currentCsvString = "";
+                Player currentCsvString= null;*/
+
+                while (zeile != null) { // null wenn am Dateiende
+                    System.out.println(zeilenCounter + ": " + zeile);
+                    zeilenCounter++;
+
+                    zeile = zeile.replace(",,", ",0,");
+                    StringTokenizer st = new StringTokenizer(zeile, ",");
+
+                    String Player = st.nextToken();
+                    boolean Spielstand= Boolean.parseBoolean(st.nextToken());
+
+                    zeile = meinReader.readLine(); // naechste Zeile
+                }
+                meinReader.close();
+
+
+
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
 
         }
 }
